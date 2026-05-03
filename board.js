955bb51b68d25e767b0907d08993b1f6386a2c7b
zeussesions <ashersimon@gmail.com>
2026-05-02 21:13:33 -0400

    it works! update for github pages

--- board.js
+++ board.js
@@ -1,32 +1,71 @@
-{
-    const range = (start, stop, step = 1) =>
-        Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+function range(start, stop, step) {
+    return Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 }
 class Board {
     constructor() {
         this.size = 9;
         this.grid = [];
-
+        this.unsolved = [];
+        // generate grid //
         for (let i = 0; i < this.size; i++) {
             this.grid[i] = new Array();
             for (let j = 0; j < this.size; j++) {
                 this.grid[i][j] = 0;
             }
         }
-    }
 
-    generate() {
-        for (let x = 0; x < this.size; x++) {
-            for (let y = 0; y < this.size; y++) {
-                let options = this.checkCell(x, y);
-
-                if (options.length == 0) {
-                    this.backtrack(x, y);
-                } else {
-                    this.setCell(x, y, options);
-                }
+        for (let i = 0; i < this.size; i++) {
+            this.unsolved[i] = new Array();
+            for (let j = 0; j < this.size; j++) {
+                this.unsolved[i][j] = 0;
             }
         }
+    }
+
+    init() {
+        for (let x = 0; x < this.size; x += 1) {
+            for (let y = 0; y < this.size; y += 1) {
+                this.grid[x][y] = 0;
+                this.unsolved[x][y] = 0;
+            }
+        }
+    }
+
+    generate(difficulty) {
+        let sudoku = this.solve(0, 0);
+        this.punchHoles(difficulty);
+        return sudoku;
+    }
+
+    solve(x, y) {
+        if (x === this.size) {
+            x = 0;
+            y++;
+        } // wrap row
+        if (y === this.size) return true;
+
+        const options = this.checkCell(x, y);
+        this.shuffle(options);
+
+        for (const num of options) {
+            this.grid[x][y] = num;
+
+            if (this.solve(x + 1, y)) {
+                return true;
+            }
+
+            this.grid[x][y] = 0;
+        }
+
+        return false;
+    }
+
+    shuffle(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
     }
 
     checkCell(cellX, cellY) {
@@ -34,22 +73,31 @@ class Board {
 
         // check row //
         for (let i = 0; i < this.size; i++) {
-            if (this.grid[i][cellY] != 0) {
-                let index = options.indexOf(this.grid[i][cellY]);
-                options.splice(index, 1);
+            if (this.grid[cellX][i] != 0) {
+                // ← cellX, vary i (column)
+                let index = options.indexOf(this.grid[cellX][i]);
+                if (index !== -1) options.splice(index, 1);
             }
         }
-
         // check column //
         for (let i = 0; i < this.size; i++) {
-            if (this.grid[cellX][i] != 0) {
-                let index = options.indexOf(this.grid[cellX][i]);
-                options.splice(index, 1);
+            if (this.grid[i][cellY] != 0) {
+                // ← vary i (row), cellY
+                let index = options.indexOf(this.grid[i][cellY]);
+                if (index !== -1) options.splice(index, 1);
             }
         }
 
         // check 3x3 //
+        let boxX = Math.floor(cellX / 3) * 3;
+        let boxY = Math.floor(cellY / 3) * 3;
 
+        for (let i = boxX; i < boxX + 3; i++) {
+            for (let j = boxY; j < boxY + 3; j++) {
+                let index = options.indexOf(this.grid[i][j]);
+                if (index !== -1) options.splice(index, 1);
+            }
+        }
         return options;
     }
 
@@ -63,6 +111,30 @@ class Board {
             console.log("on edge", backtrackX);
         }
         this.grid[backtrackX - 1][backtrackY] = 0;
+    }
+
+    // (y * width)+ x
+    punchHoles(difficulty) {
+        let clues;
+        if (difficulty == 1) {
+            clues = Math.floor(random(35, 45));
+        } else if (difficulty == 2) {
+            clues = Math.floor(random(27, 35));
+        } else if (difficulty == 3) {
+            clues = Math.floor(random(20, 27));
+        } else if (difficulty == 4) {
+            clues = 17;
+        }
+
+        let randCords = subset(shuffle(range(0, 80, 1)), 0, clues);
+        // console.log(clues);
+
+        for (let i = 0; i < randCords.length; i++) {
+            let x = randCords[i] % this.size;
+            let y = Math.floor(randCords[i] / this.size);
+            // console.log(x, y);
+            this.unsolved[x][y] = this.grid[x][y];
+        }
     }
 
     //-------------RENDER-------------//
@@ -83,8 +155,8 @@ class Board {
 
                 let textPosX = posx + width / this.size / 2;
                 let textPosY = posy + height / this.size / 2.3;
-                if (this.grid[x][y] != 0) {
-                    text(this.grid[x][y], textPosX, textPosY);
+                if (this.unsolved[x][y] != 0) {
+                    text(this.unsolved[x][y], textPosX, textPosY);
                 }
             }
         }
@@ -95,5 +167,19 @@ class Board {
         line((width / 3) * 2, 0, (width / 3) * 2, height);
         line(0, height / 3, width, height / 3);
         line(0, (height / 3) * 2, width, (height / 3) * 2);
+    }
+
+    outputText() {
+        let text = "";
+        for (let x = 0; x < this.size; x++) {
+            for (let y = 0; y < this.size; y++) {
+                if (this.unsolved[x][y] == 0) {
+                    text += "0";
+                } else {
+                    text += String(this.unsolved[x][y]);
+                }
+            }
+        }
+        return text;
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let board;
+let code;
+let title;
 
 function preload() {
     font = loadFont("/assets/Rubik-VariableFont_wght.ttf");
@@ -7,11 +9,17 @@ function preload() {
 function setup() {
     createCanvas(500, 500);
     board = new Board();
+    title = createP("Code:");
+    code = createP("");
 }
 
 function draw() {
     background(255);
-    board.generate();
+    board.init();
+    board.generate(1);
+    console.log(board.outputText());
     board.render();
+    frameRate(2);
     noLoop();
+    code.html(board.outputText());
 }
